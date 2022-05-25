9652a5ea309287e6389a2df5293015d1e3fe8d82
AyjerenR <jeren.jmv@gmail.com>
2022-05-25 10:16:44 +0200

    important functions for website building with blogdown

---
+++ Most-important-functions.R
@@ -0,0 +1,15 @@
+# Most important functions
+
+#1. Creating the site
+blogdown::new_site(theme = "Input theme name")
+
+#2. Serve site
+blogdown::serve_site()
+
+
+#3. Create a blog post
+blogdown::new_post("My New Post")
+
+#4. Build the cite (fills in the public)
+blogdown::build_site()
+
